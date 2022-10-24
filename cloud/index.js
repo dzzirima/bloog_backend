636da52a7a5a5bf903832694b3b4edf2472d38b5
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2022-10-24 21:43:33 +0200

    feat:upload done

---
+++ cloud/index.js
@@ -0,0 +1,13 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({ 
+    cloud_name: 'dkod4gxa5', 
+    api_key:'563171434972464' , 
+    api_secret: '6TK7Y7vEV12vibyc7q3aSNJdhE8',
+    secure:true
+  });
+
+
+
+
+  export default cloudinary
